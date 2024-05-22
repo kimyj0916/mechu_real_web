0121f222d105374865b2abb88338cb65740dd988
kimyj0916 <155420577+kimyj0916@users.noreply.github.com>
2024-05-23 00:26:05 +0900

    메추리알 웹 스프링부트 부분 기초코드 완성

---
+++ mRealProject/src/main/java/team/mreal/demo/MRealProjectApplication.java
@@ -0,0 +1,13 @@
+package team.mreal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MRealProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MRealProjectApplication.class, args);
+	}
+
+}
---
+++ mRealProject/src/test/java/team/mreal/demo/MRealProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package team.mreal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MRealProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
